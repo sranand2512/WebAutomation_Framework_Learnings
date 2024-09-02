328aaae2a46e67763222a137a2a3dc90dfd6be42
sanan <saurabhk2512@gmail.com>
2024-09-02 08:29:32 +0530

    day 25 jul

--- src/test/java/com/saurabhSeleniumAuto/tests/TestvwoLogin_POM.java
+++ src/test/java/com/saurabhSeleniumAuto/tests/TestvwoLogin_POM.java
@@ -3,13 +3,14 @@ package com.saurabhSeleniumAuto.tests;
 import com.saurabhSeleniumAuto.pages.pageObjectModel.loginPage_POM;
 import junit.framework.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class TestvwoLogin_POM {
     @Test
     public void testLoginNegative(){
-        WebDriver driver = new EdgeDriver();
+        WebDriver driver = new ChromeDriver();
         loginPage_POM loginPagePom = new loginPage_POM(driver);
         String errormsgText = loginPagePom.loginTovwoInvalidCred("abc@ght.com","test123");
         Assert.assertEquals(errormsgText,"Your email, password, IP address or location did not match");
@@ -19,7 +20,7 @@ public class TestvwoLogin_POM {
     }
     @Test
     public void testLoginNegative2(){
-        WebDriver driver = new EdgeDriver();
+        WebDriver driver = new ChromeDriver();
         loginPage_POM loginPagePom = new loginPage_POM(driver);
         String errormsgText = loginPagePom.loginTovwoInvalidCred("aassdsdc@ght.com","123");
         Assert.assertEquals(errormsgText,"Your email, password, IP address or location did not match");
